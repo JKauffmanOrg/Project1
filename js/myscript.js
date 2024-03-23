3bd0ca046ef8095e6fffd5111951b504a17d1f8b
JKauffman27 <jkauffman@hawk.iit.edu>
2024-03-23 16:03:44 -0400

    changing the js file to now include a form validation

--- js/myscript.js
+++ js/myscript.js
@@ -1,11 +1,26 @@
 function init(){
+  var form = document.querySelector('form');
   var button = document.getElementById('submit_button');
   
   function showText() {
-	alert("Thank you for submitting! Your ticket will be emailed to you!");
+    alert("Thank you for submitting! Your ticket will be emailed to you!");
   }
-  
-  button.addEventListener('click', showText);
+
+  function validateForm(event) {
+    var fullName = document.getElementById('full_name').value.trim();
+    var phoneNumber = document.getElementById('phone_num').value.trim();
+    var email = document.getElementById('email').value.trim();
+    var ageVerify = document.getElementById('age_verify').checked;
+
+    if (fullName === '' || phoneNumber === '' || email === '' || !ageVerify) {
+      alert("Please fill out all fields and verify your age.");
+      event.preventDefault(); 
+    } else {
+      showText();
+    }
   }
+
+  button.addEventListener('click', validateForm);
+}
 
 window.addEventListener('load', init); 
